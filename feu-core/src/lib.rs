fe06339b7dc743f271d871556fdbda7c7a0159b2
Bertrand CLAVELIER <bertrand@clavelier.me>
2026-03-05 01:18:06 +0100

    licence: applique la GPLv3-or-later à l'ensemble du projet

--- feu-core/src/lib.rs
+++ feu-core/src/lib.rs
@@ -1,3 +1,11 @@
+// Copyright (C) 2026 Bertrand CLAVELIER
+//
+// This file is part of Feu.
+//
+// Feu is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// Feu is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with Feu. If not, see <https://www.gnu.org/licenses/>.
+
 //! `feu-core` est le cœur du protocole Feu.
 //!
 //! Il expose une interface unique — la structure [`Feu`] — qui orchestre
